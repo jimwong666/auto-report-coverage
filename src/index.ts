44225a4d21cf8e0e2b062a771e20e17ffd887c3f
wangjian0818 <wangjian0818@focuschina.com>
2023-10-27 10:11:27 +0800

    feat: 更新参数和文档

--- src/index.ts
+++ src/index.ts
@@ -17,9 +17,60 @@ type Global = {
 	__coverage__: Record<string, unknown>;
 } & Record<string, unknown>;
 
-const ARC = (props: ARCprops): void => {
-	const global: Global = new Function("return this")();
+const global: Global = new Function("return this")();
 
+// 上报覆盖率
+const reportCoverage = (
+	reportURL: string,
+	coverageVariable: string,
+	params: Record<string, any>,
+) => {
+	fetch(reportURL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json;charset=utf-8",
+		},
+		body: JSON.stringify({
+			data: global[coverageVariable],
+			...global.__git_info__,
+			...params,
+		}),
+	})
+		.then((res) => {
+			if (!res.ok) {
+				throw new Error("Network response was not ok");
+			}
+			return res.json();
+		})
+		.then((json) => {
+			const { success, message } = json;
+			if (success) {
+				console.log(message);
+
+				// 重置 coverage 统计数据
+				const cocerageData = global[coverageVariable] as any;
+				for (const i in cocerageData) {
+					for (const r in cocerageData[i].b) {
+						cocerageData[i].b[r].fill(0);
+					}
+					for (const m in cocerageData[i].f) {
+						cocerageData[i].f[m] = 0;
+					}
+					for (const n in cocerageData[i].s) {
+						cocerageData[i].s[n] = 0;
+					}
+				}
+			} else {
+				throw new Error(message);
+			}
+		})
+		.catch((err) => console.log("Request Failed", err));
+};
+
+const ARC = (props: ARCprops): void => {
+	// 自动上报覆盖率
+
+	// 1、定时上报
 	const {
 		interval = 5 * 60 * 1000,
 		// 开发环境 reportURL
@@ -29,44 +80,13 @@ const ARC = (props: ARCprops): void => {
 	} = props || {};
 
 	setInterval(() => {
-		fetch(reportURL, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json;charset=utf-8",
-			},
-			body: JSON.stringify({
-				data: global[coverageVariable],
-				remote: global.__git_info__.remote,
-				branch: global.__git_info__.branch,
-				project_name: global.__git_info__.project_name,
-				...params,
-			}),
-		})
-			.then((res) => res.json())
-			.then((json) => {
-				const { success, message } = json;
-				if (success) {
-					console.log(message);
-
-					// 重置 coverage 统计数据
-					const cocerageData = global[coverageVariable] as any;
-					for (const i in cocerageData) {
-						for (const r in cocerageData[i].b) {
-							cocerageData[i].b[r].fill(0);
-						}
-						for (const m in cocerageData[i].f) {
-							cocerageData[i].f[m] = 0;
-						}
-						for (const n in cocerageData[i].s) {
-							cocerageData[i].s[n] = 0;
-						}
-					}
-				} else {
-					console.error(message);
-				}
-			})
-			.catch((err) => console.log("Request Failed", err));
+		reportCoverage(reportURL, coverageVariable, params);
 	}, interval);
+
+	// 2、页面关闭时上报
+	window.onbeforeunload = () => {
+		reportCoverage(reportURL, coverageVariable, params);
+	};
 };
 
 export default ARC;
